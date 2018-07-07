e0a01ff66d13d7a3a639eb07a8f26b46da46f707
Andy <ajlewis1@me.com>
2018-07-06 18:37:56 -0600

    Initial Commit

---
+++ One_Away/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  One_Away
+//
+//  Created by Andrew Lewis on 7/6/18.
+//  Copyright © 2018 Andrew Lewis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
